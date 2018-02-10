3eb02beacc765fc32293e10012bc9a90eb1f8294
BenSibley <ben@competethemes.com>
2018-02-09 19:27:26 -0500

    renamed meta box file

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ require_once( trailingslashit( get_template_directory() ) . 'theme-options.php' 
 require_once( trailingslashit( get_template_directory() ) . 'inc/widgets/post-list.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/comments.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/customizer.php' );
-require_once( trailingslashit( get_template_directory() ) . 'inc/meta-box.php' );
+require_once( trailingslashit( get_template_directory() ) . 'inc/meta-box-layout.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/review.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/scripts.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/social-icons.php' );

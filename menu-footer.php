6ecdf0cddab8fc543e3d2803f2b4acf2985c5e07
BenSibley <ben@competethemes.com>
2018-10-22 08:44:44 -0400

    removed fallback from footer menu

--- menu-footer.php
+++ menu-footer.php
@@ -4,6 +4,7 @@
 			'theme_location'  => 'footer',
 			'container'       => 'nav',
 			'container_class' => 'menu',
+			'fallback_cb'			=> false,
 			'menu_class'      => 'menu-footer-items',
 			'menu_id'         => 'menu-footer-items',
 			'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>'

1fb772c78c1e2360336fc4630e9de180cce54897
BenSibley <ben@competethemes.com>
2020-07-16 11:37:29 -0400

    compatibility fix with MailOptin's form customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -1314,7 +1314,7 @@ function ct_mission_news_sanitize_phone( $input ) {
 
 
 function ct_mission_news_customize_preview_js() {
-	if ( !defined( 'MISSION_NEWS_PRO_FILE' ) ) {
+	if ( !defined( 'MISSION_NEWS_PRO_FILE' ) && !(isset($_GET['mailoptin_optin_campaign_id']) || isset($_GET['mailoptin_email_campaign_id'])) ) {
 		$url = 'https://www.competethemes.com/mission-news-pro/?utm_source=wp-dashboard&utm_medium=Customizer&utm_campaign=Mission%20News%20Pro%20-%20Customizer';
 		$content = "<script>jQuery('#customize-info').prepend('<div class=\"upgrades-ad\"><a href=\"". $url ."\" target=\"_blank\">Customize More - Mission News Pro <span>&rarr;</span></a></div>')</script>";
 		echo apply_filters('ct_mission_news_customizer_ad', $content);

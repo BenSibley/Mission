7a0ad9da2c1c3563c1ecdea18c4db7051de63f65
BenSibley <ben@competethemes.com>
2024-07-10 10:53:12 -0400

    Removed review request

--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@ require_once(trailingslashit(get_template_directory()) . 'inc/customizer.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/last-updated-meta-box.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/meta-box-layout.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/meta-box-fi-display.php');
-require_once(trailingslashit(get_template_directory()) . 'inc/review.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/scripts.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/social-icons.php');
 // TGMP
@@ -51,18 +50,6 @@ function ct_mission_news_register_required_plugins()
 }
 add_action('tgmpa_register', 'ct_mission_news_register_required_plugins');
 
-//----------------------------------------------------------------------------------
-//	Include review request
-//----------------------------------------------------------------------------------
-require_once(trailingslashit(get_template_directory()) . 'dnh/handler.php');
-new WP_Review_Me(
-    array(
-        'days_after' => 14,
-        'type'       => 'theme',
-        'slug'       => 'mission-news',
-        'message'    => __('Hey! Sorry to interrupt, but you\'ve been using Mission News for a little while now. If you\'re happy with this theme, could you take a minute to leave a review? <i>You won\'t see this notice again after closing it.</i>', 'mission-news')
-    )
-);
 //----------------------------------------------------------------------------------
 //	Set content width variable
 //----------------------------------------------------------------------------------
